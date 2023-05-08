474b38fc6b58e0eac77b11fe6b4715af04499d22
Harry <hieu.nguyen@sotatek.com>
2023-05-08 18:07:19 +0700

    add test suite

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -10,7 +10,7 @@ public class BaseTest {
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
         BaseApi.initReqSpec();
-        HttpRequest.setBaseUrl("http://172.16.1.230:8033/api/v1/");
+        HttpRequest.setBaseUrl("http://10.4.10.231:8034/api/v1/");
     }
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
