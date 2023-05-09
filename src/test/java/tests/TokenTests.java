86d7cc132c9f1bde255105c218d48028827a4723
Sotatek-DavidNguyen4 <david.nguyen4@sotatek.com>
2023-05-09 17:01:09 +0700

    create folder

--- src/test/java/tests/TokenTests.java
+++ src/test/java/tests/TokenTests.java
@@ -5,7 +5,7 @@ import microservices.steps.token.service.TokenSteps;
 import org.testng.annotations.Test;
 
 public class TokenTests extends BaseTest {
-    @Test
+    @Test(description = "verify that get token successfull", groups={"token"})
     public void getListToken(){
         TokenSteps tokenSteps = new TokenSteps();
         tokenSteps.getListTokens();
