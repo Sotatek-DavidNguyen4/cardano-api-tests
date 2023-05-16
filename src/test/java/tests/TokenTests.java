4c6792a2ceec8cd2401e3f502249ac0c3b8be132
Sotatek-DavidNguyen4 <david.nguyen4@sotatek.com>
2023-05-16 09:35:23 +0700

    edit code

--- src/test/java/tests/TokenTests.java
+++ src/test/java/tests/TokenTests.java
@@ -118,9 +118,7 @@ public class TokenTests extends BaseTest {
     public Object[][] DatasetWithTokenIdInvalid(){
         return new Object[][]{
                 {"asset1ee0u29k4xwauf0r7w8g30klgraxw0y4rz2t"},
-                {null},
                 {"@#$%=&"},
-                {""},
                 {" "}
         };
     }
