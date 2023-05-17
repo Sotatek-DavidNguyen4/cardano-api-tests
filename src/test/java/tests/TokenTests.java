c94e8cbc18b8d5a1237ce2a340df0d207cd7d655
Sotatek-DavidNguyen4 <david.nguyen4@sotatek.com>
2023-05-17 18:20:44 +0700

    get token mints with param invalid

--- src/test/java/tests/TokenTests.java
+++ src/test/java/tests/TokenTests.java
@@ -169,4 +169,49 @@ public class TokenTests extends BaseTest {
                 {1, 5, "id,DESC"},
         };
     }
+    @Test(description = "get token mints invalid param", groups = {"token"}, dataProvider = "paramTokenMintsParamInvalid")
+    public void getTokenMintsInvalidParam(Object page, Object size, String sort){
+        Map<String, Object> param = new CreateParameters()
+                .withPage(page)
+                .withPageSize(size)
+                .withSort(sort)
+                .getParamsMap();
+        tokenSteps.getTokenTxsParamInvalid("asset1d9v7aptfvpx7we2la8f25kwprkj2ma5rp6uwzv", param)
+                .validateResponse(HttpURLConnection.HTTP_OK);
+    }
+    @DataProvider(name = "paramTokenMintsParamInvalid")
+    public Object[][] DatasetTokenMintsParamInvalid(){
+        return new Object[][]{
+                // data for page invalid
+                {" ", 5, "id,ASC"},
+                {"", 5, "id,ASC"},
+                {null, 5, "id,ASC"},
+                {"abc", 5, "id,ASC"},
+                {"@#$", 5, "id,ASC"},
+
+                {" ", 5, "id,DESC"},
+                {"", 5, "id,DESC"},
+                {null, 5, "id,DESC"},
+                {"abc", 5, "id,DESC"},
+                {"@#$", 5, "id,DESC"},
+                //data for size invalid
+                {0, null, "id,ASC"},
+                {0, "", "id,ASC"},
+                {0, " ", "id,ASC"},
+                {0, "abc", "id,ASC"},
+                {0, "@#$", "id,ASC"},
+
+                {0, null, "id,DESC"},
+                {0, "", "id,DESC"},
+                {0, " ", "id,DESC"},
+                {0, "abc", "id,DESC"},
+                {0, "@#$", "id,DESC"},
+                //data for sort invalid
+                {1, 5, ""},
+                {1, 5, " "},
+                {1, 5, null},
+                {1, 5, "abc"},
+                {1, 5, "@#$"}
+        };
+    }
 }
