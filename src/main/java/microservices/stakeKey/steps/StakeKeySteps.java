ff89c1a656e6c079cfb5acce0ae03e5ac7dfd4ef
Sotatek-DavidNguyen4 <david.nguyen4@sotatek.com>
2023-05-23 14:57:09 +0700

    edit code

--- src/main/java/microservices/stakeKey/steps/StakeKeySteps.java
+++ src/main/java/microservices/stakeKey/steps/StakeKeySteps.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Step;
 import microservices.common.steps.BaseSteps;
 
 import javax.xml.ws.Endpoint;
+import java.util.Map;
 
 public class StakeKeySteps extends BaseSteps {
     @Step("get a stake detail by address")
--- src/test/java/tests/StakeKeyTests.java
+++ src/test/java/tests/StakeKeyTests.java
@@ -5,18 +5,20 @@ import microservices.stakeKey.steps.StakeKeySteps;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.net.HttpURLConnection;
 public class StakeKeyTests extends BaseTest {
     private StakeKeySteps stakeKeySteps = new StakeKeySteps();
+    private String address = "addr_test1qr53akzyd4949txn5hu583yu0xatcvp2efec9tm56jpeg6xkfjf77qy57hqhnefcqyy7hmhsygj9j38rj984hn9r57fsq48dyr";
+
     @Test(description = "get a stake detail by address", groups = {"stakeKey"})
     public void getStakeByAddress(){
-        String address = "addr_test1qr53akzyd4949txn5hu583yu0xatcvp2efec9tm56jpeg6xkfjf77qy57hqhnefcqyy7hmhsygj9j38rj984hn9r57fsq48dyr";
         stakeKeySteps.getStakeByAddress(address)
-                .validateStatusCode(200);
+                .validateStatusCode(HttpURLConnection.HTTP_OK);
     }
     @Test(description = "get stake detail by address wrong format", groups = {"stakeKey"}, dataProvider = "listAddressWrongFormat")
     public void getStakeByAddresWrongFormat(Object address){
         stakeKeySteps.getStakeByAddress(address)
-                .validateStatusCode(400);
+                .validateStatusCode(HttpURLConnection.HTTP_BAD_REQUEST);
     }
     @DataProvider(name = "listAddressWrongFormat")
     public Object[][] DatasetListAddress(){
