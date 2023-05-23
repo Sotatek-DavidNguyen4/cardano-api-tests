9b3604f8a9b4c94399f1ee7f97751c67e8ab329f
Sotatek-DavidNguyen4 <david.nguyen4@sotatek.com>
2023-05-23 09:46:14 +0700

    declare an variable String address

--- src/test/java/tests/StakeKeyTests.java
+++ src/test/java/tests/StakeKeyTests.java
@@ -9,7 +9,8 @@ public class StakeKeyTests extends BaseTest {
     private StakeKeySteps stakeKeySteps = new StakeKeySteps();
     @Test(description = "get a stake detail by address", groups = {"stakeKey"})
     public void getStakeByAddress(){
-        stakeKeySteps.getStakeByAddress("addr_test1qr53akzyd4949txn5hu583yu0xatcvp2efec9tm56jpeg6xkfjf77qy57hqhnefcqyy7hmhsygj9j38rj984hn9r57fsq48dyr")
+        String address = "addr_test1qr53akzyd4949txn5hu583yu0xatcvp2efec9tm56jpeg6xkfjf77qy57hqhnefcqyy7hmhsygj9j38rj984hn9r57fsq48dyr";
+        stakeKeySteps.getStakeByAddress(address)
                 .validateStatusCode(200);
     }
     @Test(description = "get stake detail by address wrong format", groups = {"stakeKey"}, dataProvider = "listAddressWrongFormat")
