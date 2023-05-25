9ce36c1779e51d4e988e0eb5d2bf2cd47011d1da
Harry <hieu.nguyen@sotatek.com>
2023-05-25 10:22:22 +0700

    verify get block detail

--- src/test/java/tests/block/GetAllBlockTests.java
+++ src/test/java/tests/block/GetAllBlockTests.java
@@ -1,25 +1,21 @@
 package tests.block;
 
 import base.BaseTest;
-import constants.enums.AnalyticsType;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import microservices.block.models.BlockModels;
-import microservices.block.steps.BockSteps;
+import microservices.block.steps.BlockSteps;
 import org.apache.commons.collections.MultiMap;
-import org.apache.commons.collections.map.MultiValueMap;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import util.CreateMultiParameters;
-import util.CreateParameters;
 
 import java.net.HttpURLConnection;
-import java.util.Map;
 
 @Epic("cardano")
 @Feature("api-block")
 public class GetAllBlockTests extends BaseTest {
-    BockSteps bockSteps = new BockSteps();
+    BlockSteps bockSteps = new BlockSteps();
     @Test(description = "Get all blocks successfully", groups = {"block", "get-all-blocks"}, dataProvider = "paramData")
     public void getAllBlockSuccessfully(Object page, Object size, Object sort){
 
