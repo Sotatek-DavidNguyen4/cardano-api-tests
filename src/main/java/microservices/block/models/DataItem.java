15448bf2ce2e0ab732a7aac4b97ef1d5516488a7
Harry <hieu.nguyen@sotatek.com>
2023-05-29 11:01:28 +0700

    get list of block

--- src/main/java/microservices/block/models/DataItem.java
+++ src/main/java/microservices/block/models/DataItem.java
@@ -1,10 +1,12 @@
 package microservices.block.models;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 import java.util.List;
 @Data
 public class DataItem{
+	private int totalFees;
 	private String blockHash;
 	private long totalOutput;
 	private int epochNo;
@@ -16,4 +18,9 @@ public class DataItem{
 	private String time;
 	private List<String> addressesOutput;
 	private String hash;
+	private int txCount;
+	private String slotLeader;
+	@SerializedName("time")
+	private String id;
+	private int slotNo;
 }
