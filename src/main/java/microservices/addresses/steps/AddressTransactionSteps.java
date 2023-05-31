fd550cd2222ab16a3e6fadf8ae7e37bbf9ff787c
Harry <hieu.nguyen@sotatek.com>
2023-05-31 09:24:28 +0700

    update address group

--- src/main/java/microservices/addresses/steps/AddressTransactionSteps.java
+++ src/main/java/microservices/addresses/steps/AddressTransactionSteps.java
@@ -6,6 +6,11 @@ import microservices.addresses.constants.AddressConstants;
 import microservices.addresses.models.AddressTransactionModel;
 import microservices.common.steps.BaseSteps;
 import org.testng.Assert;
+import util.AttributeStandard;
+
+import java.util.stream.Collectors;
+
+import static constants.DateFormats.DATE_FORMAT;
 
 public class AddressTransactionSteps extends BaseSteps {
     @Step("get the transaction of address")
@@ -23,6 +28,13 @@ public class AddressTransactionSteps extends BaseSteps {
     public AddressTransactionSteps verifyAddressInputIsSameAsOutputData(AddressTransactionModel actualAddressInput, String expectedAddressOutput){
         boolean flag = actualAddressInput.getData().stream().map(s -> s.getAddressesInput()).anyMatch(x -> x.listIterator().next().equals(expectedAddressOutput));
         Assert.assertTrue(flag,"all address input is same as input data");
+        return this;
+    }
+    @Step("verify format attributes")
+    public AddressTransactionSteps verifyFormatAttributes(AddressTransactionModel actualAddressInput){
+        Assert.assertTrue(AttributeStandard.areValidHashes(actualAddressInput.getData().stream().map(s -> s.getHash()).collect(Collectors.toList())));
+        Assert.assertTrue(AttributeStandard.areValidHashes(actualAddressInput.getData().stream().map(s -> s.getBlockHash()).collect(Collectors.toList())));
+        Assert.assertTrue(AttributeStandard.areValidDates(actualAddressInput.getData().stream().map(s -> s.getTime()).collect(Collectors.toList()), DATE_FORMAT[0]));
         return this;
     }
 }
