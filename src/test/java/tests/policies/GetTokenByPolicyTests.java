63b7fc5ca1ac33a488401fc15b0714b50a112e47
SotaTek-Binh Le <Binh Le>
2023-06-16 09:29:08 +0700

    update policy test

--- src/test/java/tests/policies/GetTokenByPolicyTests.java
+++ src/test/java/tests/policies/GetTokenByPolicyTests.java
@@ -1,12 +1,15 @@
 package tests.policies;
 
 import base.BaseTest;
+import constants.Endpoints;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import microservices.policy.models.token.TokenByPolicy;
 import microservices.policy.models.token.TokenByPolicyData;
 import microservices.policy.steps.PolicySteps;
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -28,14 +31,14 @@ public class GetTokenByPolicyTests extends BaseTest {
                 .saveResponseObject(TokenByPolicy.class);
 
         tokenByPolicyData = tokenByPolicy.getData();
-        policySteps.verifyResponseDataOfToken(tokenByPolicyData,"4b555431","KUT1",109954)
-                   .verifyFormatOfGetTokensByPolicies(tokenByPolicyData)
-                   .verifyPolicyInResponseDataOfToken(tokenByPolicyData,policyId);
+        policySteps.verifyFormatOfGetTokensByPolicies(tokenByPolicyData)
+                   .verifyPolicyInResponseDataOfToken(tokenByPolicyData,policyId)
+                   .verifySizeResponseDataOfToken(tokenByPolicyData,1);
         }
     @DataProvider(name="getTokenByPolicies")
     public Object[][] getTokenByPolicies(){
         return new Object[][]{
-                {"e38748c08c510a4a5d712922a0f91269b8446ac565068f653c517475"},
+                {"a0028f350aaabe0545fdcb56b039bfb08e4bb4d8c4d7c3c7d481c235"},
         };
     }
 
@@ -47,7 +50,7 @@ public class GetTokenByPolicyTests extends BaseTest {
                 .saveResponseObject(TokenByPolicy.class);
 
         tokenByPolicyData = tokenByPolicy.getData();
-        policySteps.verifySizeResponseDataOfToken(tokenByPolicyData,3)
+        policySteps.verifySizeResponseDataOfToken(tokenByPolicyData,20)
                    .verifyResponseDataOfListTokenNotNull(tokenByPolicyData)
                    .verifyFormatOfGetTokensByPolicies(tokenByPolicyData)
                    .verifyPolicyInResponseDataOfToken(tokenByPolicyData,policyId);
@@ -56,7 +59,7 @@ public class GetTokenByPolicyTests extends BaseTest {
     @DataProvider(name="getListTokenByPoliciesSuccess")
     public Object[][] getListTokenByPoliciesSuccess(){
         return new Object[][]{
-                {"fa1ab2cbdca59874005d9186b85245ed2503acaa63ab4121ab7c3879"},
+                {"4429f7b432125357388b1d676c2d503b6d6fc78c414934bef9882e26"},
         };
     }
 
@@ -80,4 +83,60 @@ public class GetTokenByPolicyTests extends BaseTest {
                 {"asset1c0vymmx0nysjaa8q5vah78jmuqyew3kjm48azr"},
         };
     }
+
+    @Test(description = "verify get list tokens by policy with param", groups={"policy"},dataProvider = "getListTokenByPoliciesWithParams")
+    public void getListTokenByPoliciesWithParam(Object page,Object size){
+        String policyId ="4429f7b432125357388b1d676c2d503b6d6fc78c414934bef9882e26";
+
+        MultiMap params = new MultiValueMap();
+        params.put("page", page);
+        params.put("size", size);
+
+        tokenByPolicy = (TokenByPolicy) policySteps.getTokenByPoliciesWithParams(policyId,params)
+                .validateResponse(HttpURLConnection.HTTP_OK)
+                .saveResponseObject(TokenByPolicy.class);
+
+        tokenByPolicyData = tokenByPolicy.getData();
+        policySteps.then_verifyTokenByPolicyResponse(tokenByPolicy,params)
+                .verifyResponseDataOfListTokenNotNull(tokenByPolicyData)
+                .verifyFormatOfGetTokensByPolicies(tokenByPolicyData)
+                .verifyPolicyInResponseDataOfToken(tokenByPolicyData,policyId);;
+    }
+
+    @DataProvider(name="getListTokenByPoliciesWithParams")
+    public Object[][] getListTokenByPoliciesWithParams(){
+        return new Object[][]{
+                {"10",""},
+                {"abc",""},
+                {"-10",""},
+                {" ",""},
+                {"@#$",""},
+                {"","10"},
+                {"","abc"},
+                {"","-10"},
+                {""," "},
+                {"","!@@$$"},
+        };
+    }
+
+    @Test(description = "verify get list tokens by policy with totalPage", groups={"policy"})
+    public void getListTokenByPoliciesWithParamTotalPage(){
+        String policyId ="4429f7b432125357388b1d676c2d503b6d6fc78c414934bef9882e26";
+        tokenByPolicy = (TokenByPolicy) policySteps.getTokenByPolicies(policyId)
+                .validateResponse(HttpURLConnection.HTTP_OK)
+                .saveResponseObject(TokenByPolicy.class);
+        MultiMap params = new MultiValueMap();
+        params.put("page", tokenByPolicy.getTotalPages()+1);
+        params.put("size", null);
+
+        tokenByPolicy = (TokenByPolicy) policySteps.getTokenByPoliciesWithParams(policyId,params)
+                .validateResponse(HttpURLConnection.HTTP_OK)
+                .saveResponseObject(TokenByPolicy.class);
+
+        tokenByPolicyData = tokenByPolicy.getData();
+        System.out.println(tokenByPolicyData);
+        policySteps.verifySizeResponseDataOfToken(tokenByPolicyData,0);
+
+    }
+
 }
