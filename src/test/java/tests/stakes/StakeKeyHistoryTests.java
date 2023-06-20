de40533ba191e47c484d6b2fba9664a50af4b79b
Sotatek-DavidNguyen4 <david.nguyen4@sotatek.com>
2023-06-20 14:39:45 +0700

    stake history update

--- src/test/java/tests/stakes/StakeKeyHistoryTests.java
+++ src/test/java/tests/stakes/StakeKeyHistoryTests.java
@@ -40,33 +40,6 @@ public class StakeKeyHistoryTests extends BaseTest {
                 .verifyStakeHistory(stakeHistory.getData())
                 .verifyElementsIsNotDecimal(elements);
     }
-//    @Test(description = "get stake history all key", groups = {"stake", "stake_history"})
-//    public void getStakeHistoryAllKey(){
-//        MultiMap param = new CreateMultiParameters()
-//                .withPage("0")
-//                .withPageSize("20")
-//                .getParamsMap();
-//        StakeHistory stakeHistory = (StakeHistory) stakeKeySteps
-//                .getStakeHistoryAllKey(stake, param)
-//                .validateStatusCode(HttpURLConnection.HTTP_OK)
-//                .saveResponseObject(StakeHistory.class);
-//        stakeKeySteps.then_verifyPageStakeHistoryResponse(stakeHistory, param);
-//    }
-//    @Test(description = "get stake history with invalid stake key", groups = {"stake", "stake_history"}, dataProvider = "stakeKeyInvalid")
-//    public void getStakeHistoryInvalidStake(Object stakeKey){
-//        stakeKeySteps
-//                .getStakeHistory(stakeKey)
-//                .then_verifyErrorResponse(HttpURLConnection.HTTP_BAD_REQUEST, StakeKeyConstants.ERROR_MESSAGE,StakeKeyConstants.ERROR_CODE);
-//    }
-//    @DataProvider(name = "stakeKeyInvalid")
-//    public Object[][] DatasetStakeInvalid(){
-//        return new Object[][]{
-//                {"@#$"},
-//                {" "},
-//                {"abcd"},
-//                {"12345"}
-//        };
-//    }
     @Test(description = "get stake history with invalid page", groups = {"stake", "stake_history"}, dataProvider = "page")
     public void getStakeHistoryInvalidPage(Object page){
         MultiMap params = new CreateMultiParameters()
@@ -87,18 +60,6 @@ public class StakeKeyHistoryTests extends BaseTest {
                 {"  "},
                 {"@#$"},
         };
-    }
-    @Test(description = "get stake history with invalid page = totalPage + 1", groups = {"stake", "stake_history"}, priority = 1)
-    public void getStakeHistoryPage(){
-        MultiMap params = new CreateMultiParameters()
-                .withPage(""+numberPage+"")
-                .getParamsMap();
-        StakeHistory stakeHistory = (StakeHistory) stakeKeySteps
-                .getStakeHistoryAllKey(stake, params)
-                .validateStatusCode(HttpURLConnection.HTTP_OK)
-                .saveResponseObject(StakeHistory.class);
-        stakeKeySteps.then_verifyPageStakeHistoryResponse(stakeHistory, params)
-                .then_verifySizeStakeHistoryResponse(stakeHistory, params, 0);
     }
     @Test(description = "get stake history with size", groups = {"stake", "stake_history"}, dataProvider = "size")
     public void getStakeHistoryWithSize(Object size){
